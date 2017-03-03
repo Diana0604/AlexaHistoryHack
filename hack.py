5714e4cdfa867f0214ac15262baf9921cca656f2
Diana0604 <diana.vallverdu@gmail.com>
2017-03-03 17:38:39 +0100

    prova per la cami

--- hack.py
+++ hack.py
@@ -1,20 +1,17 @@
 import urllib2
-cami = False
-if(cami == True) : user_agent = 'Mozilla/5.0 (Windows; Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-#else : user_agent = 'Mozilla/5.0 (Windows; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-#headers = { 'User-Agent' : user_agent }
+user_agent = 'Mozilla/5.0 (Windows; Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
+headers = { 'User-Agent' : user_agent }
 req = urllib2.Request('http://alexa.amazon.co.uk/spa/index.html#settings/dialogs')
 response = urllib2.urlopen(req)
 page = response.read()
 response.close() # its always safe to close an open connection
 	
 
-file = open("testfile.txt","w") 
- 
-file.write(page)
+#file = open("testfile.txt","w") 
+#file.write(page)
+#file.close() 
+#print (page)
 
-file.close() 
-print (page)
 # while loop to refresh the html
 
 import time 
